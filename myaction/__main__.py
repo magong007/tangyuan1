7e94f70412eb58932b73e8ce578d9a0810f00fa0
magong007 <75633224+magong007@users.noreply.github.com>
2024-10-03 17:27:12 -0500

    Update __main__.py

--- myaction/__main__.py
+++ myaction/__main__.py
@@ -1,1 +1,11 @@
+import argparse
 
+parser = argparse.ArgumentParser(__name__)
+
+parser_github = parser.add_argument_group("github")
+parser_github.add_argument_group("-gt","--github-token")
+parser_github.add_argument_group("-gr","--github-repository")
+
+if __name__ == "__main__":
+  arguments = parser.parse_args()
+  print(f" >> {arguments.github_repository}")
